ca352844637e2d5f1391fd55f68e5755de116b31
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-07 21:49:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,18 @@
 class Dog
   
   def name=(dog_name)
-    @this_name = dog_name
+    @name = dog_name
   end
   
   def name
-    @this_name
+    @name
   end
   
   def breed=(dog_breed)
-    @this_breed = dog_breed
+    @breed = dog_breed
   end
   
   def breed
-    @this_breed
+    @breed
   end
 end
