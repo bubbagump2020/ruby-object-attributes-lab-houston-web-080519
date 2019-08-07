865c40081da14c8a7f035a104e6d4cf1faa4d241
Kevin Bagnall <bubbagump2020@gmail.com>
2019-08-07 21:52:01 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+class Person
+  def person_name=(individual)
+    @name = individual
+  end
+  def person_name
+    @name
+  end
+  
+  def career=(person_job)
+    @job = person_job
+  end
+  def career
+    @job
+  end
+  
+end
